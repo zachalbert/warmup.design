f1aa31925e602db765510c7b8f62fe9cf722fe18
zachalbert <zachalbert@gmail.com>
2015-10-19 09:15:06 -0700

    Make fixtures.js work for Warmups

--- README.md
+++ README.md
@@ -4,16 +4,17 @@ A daily design warmup to help designers get better at design.
 
 # TODO
 
-Everything, more or less.
-
-* Make `server/fixtures.js` work
 * Remove autopublish package and get the right things publishing in `server/publications.js`
 * Figure out how to use flow router and flow layout (already included) from (this site)[http://massimilianomarini.com/flowrouter-and-flowlayout-101/].
+* Get fixtures for prompts working.
+
+# Setup
 
 * If you don't have meteor installed, run `curl https://install.meteor.com/ | sh`
 * Clone the repo
 * Run `meteor` from the app directory, which will serve the app to http://localhost:3000
 * Run `meter mongo` to do database stuff directly
+* Currently no fixtures for adding new prompts, which must be done via the mongo command line directly. `server/fixtures.js` will add 5 random Warmup submissions, however.
 
 # Dummy data
 
--- server/fixtures.js
+++ server/fixtures.js
@@ -1,16 +1,26 @@
 /* Adds dummy data to db */
-// function randomDate(start, end) {  
-//     return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
-// }
 
-// Meteor.startup(function () {  
-//   if (Posts.find().count() === 0) {
-//     _(5).times(function (n) {
-//       Posts.insert({
-//         title: 'Title ' + n,
-//         body: 'Content body ' + n,
-//         createdAt: randomDate(new Date(2015, 0, 1), new Date())
-//       });
-//     });
-//   }
-// });
+var numWarmups = 5;
+
+function randomDate(start, end) {  
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+}
+
+function randomImageParams() {
+  return Math.round((Math.random()*1500+500)) + '/' + Math.round((Math.random()*1500+500));
+}
+
+Meteor.startup(function () {  
+  if (Warmups.find().count() === 0) {
+    _(numWarmups).times(function (n) {
+      Warmups.insert({
+        imageURL: 'http://www.fillmurray.com/' + randomImageParams(),
+        submitter: 'Mister Submitter ' + n,
+        submitterURL: 'http://www.asdf.com/',
+        shareCount: Math.round((Math.random()*25)+1),
+        likeCount: Math.round((Math.random()*25)+1),
+        createdAt: randomDate(new Date(2015, 0, 1), new Date())
+      });
+    });
+  }
+});
