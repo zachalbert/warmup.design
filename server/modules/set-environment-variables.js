2bfba58e3d7d657a5e92d043bd95db926a551734
zachalbert <zachalbert@gmail.com>
2015-11-02 10:49:44 -0800

    Remove mail_url env variable until I can get it working

--- server/modules/set-environment-variables.js
+++ server/modules/set-environment-variables.js
@@ -1,5 +1,6 @@
 let setEnvironmentVariables = () => {
-  process.env.MAIL_URL = Meteor.settings.private.MAIL_URL;
+// TODO: Base boilerplate: Was breaking things, will uncomment when I can get it working (zh)
+//   process.env.MAIL_URL = Meteor.settings.private.MAIL_URL;
 };
 
 Modules.server.setEnvironmentVariables = setEnvironmentVariables;
